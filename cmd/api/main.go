5e517b6c9aab71b7595e6ac2bf9e868c9d5a611c
phoenixixixix <barchukoleg12@gmail.com>
2024-03-09 19:10:47 +0200

    Setup skeleton HTTP server with basic configurations

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0" // Current API version. TODO: generate version automatically at build time
+
+// Will be used to hold config settings for the app.
+type config struct {
+	port int
+	env  string
+}
+
+// This is defined to hold dependencies for HTTP handlers, helpers, and middleware
+type application struct {
+	config config      // copy
+	logger *log.Logger // log object
+}
 
 func main() {
-	fmt.Println("Hello World")
+	var cfg config // new instance
+
+	// Flags which can be specified on launch
+	flag.IntVar(&cfg.port, "port", 8080, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse() // Must be called affter all fags are defined
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime) // *log.Logger
+
+	// dependencies
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// servmux declaration
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	// HTTP server configuration
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Minute,
+		WriteTimeout: 30 * time.Minute,
+	}
+
+	// Launch srv
+	logger.Printf("Starting %s server on %s port", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
 }

a485c57667b9eabbdd68cc6672eac8de8773681c
phoenixixixix <barchukoleg12@gmail.com>
2024-03-10 20:53:56 +0200

    Health check responce in JSON

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,7 +6,10 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	// Making json like string, because in the end of the day json is just a text
+	js := `{"status": "available", "environment": %q, "version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version) // papulating interpolated values
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(js))
 }

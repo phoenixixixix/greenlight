9819f58d9cbd16ff137ce923f4f358e0b1707f4b
phoenixixixix <barchukoleg12@gmail.com>
2024-03-20 17:24:43 +0200

    Add middleware for Postgres and establish connection pool

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0" // Current API version. TODO: generate version automatically at build time
@@ -15,6 +19,9 @@ const version = "1.0.0" // Current API version. TODO: generate version automatic
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 // This is defined to hold dependencies for HTTP handlers, helpers, and middleware
@@ -29,9 +36,18 @@ func main() {
 	// Flags which can be specified on launch
 	flag.IntVar(&cfg.port, "port", 8080, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	// Postgres options: User: greenlight, DB: greenlight, password: pa55word, ssl disabled
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:pa55word@localhost/greenlight?sslmode=disable", "PostgreSQL DSN")
 	flag.Parse() // Must be called affter all fags are defined
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime) // *log.Logger
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
+	logger.Printf("database connection pool established")
 
 	// dependencies
 	app := &application{
@@ -50,6 +66,27 @@ func main() {
 
 	// Launch srv
 	logger.Printf("Starting %s server on %s port", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+// Returns connection pool
+func openDB(cfg config) (*sql.DB, error) {
+	// create empty connection pool
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// Here I check if connection is establiched. If connection couldn't be
+	// establiched successfully within 5 second - return an error
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// If connection establiched retrun pool
+	return db, nil
 }
